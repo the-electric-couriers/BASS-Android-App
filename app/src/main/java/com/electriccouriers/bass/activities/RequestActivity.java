5ce6cc638c5f1736fef7e285f5e845ebbc71161e
TB <tanpantaan@gmail.com>
2019-03-25 11:10:37 +0100

    no message

---
+++ app/src/main/java/com/electriccouriers/bass/activities/RequestActivity.java
@@ -0,0 +1,81 @@
+package com.electriccouriers.bass.activities;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupMenu;
+import android.widget.Toast;
+
+import com.electriccouriers.bass.R;
+
+public class RequestActivity extends BaseActivity {
+
+    Button beginLocationButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_request);
+
+        beginLocationButton = (Button) findViewById(R.id.BeginLocation);
+        beginLocationButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View V)  {
+
+            PopupMenu pm = new PopupMenu (RequestActivity.this,beginLocationButton);
+            pm.getMenuInflater().inflate(R.menu.popup_menu, pm.getMenu());
+            pm.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem BeginLocationButton) {
+                    switch (item.getItemId() {
+                        case R.id.first:
+                            Toast.makeText(RequestActivity.this, "Geselecteerd!", Toast.LENGTH_SHORT.show();
+                            return true;
+
+                        case R.id.second:
+                            Toast.makeText(RequestActivity.this, "Geselecteerd!", Toast.LENGTH_SHORT.show();
+                            return true;
+
+                        case R.id.third:
+                            Toast.makeText(RequestActivity.this, "Geselecteerd!", Toast.LENGTH_SHORT.show();
+                            return true;
+                    }
+
+                    return true;
+
+                }
+            });
+                            pm.show();
+
+
+
+    @Override
+    protected int getLayoutResourceId() {
+        return R.layout.activity_request;
+    }
+
+    @Override
+    protected int getToolbarTitle() {
+        return 0;
+    }
+
+    @Override
+    protected int getToolbarNavigationIcon() {
+        return 0;
+    }
+
+    @Override
+    protected int getOptionsMenu() {
+        return 0;
+    }
+}
+
+    });
+
+
+
+
+
+
