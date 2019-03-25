1ce04a7f56d18e61f34265ebe25e7c75fc8ad0f3
NikkiValkenburg <nikki.valkenburg@gmail.com>
2019-03-25 14:28:52 +0100

    Finished the rest of the CardHistoryListAdapter

--- app/src/main/java/com/electriccouriers/bass/adapters/CardHistoryListAdapter.java
+++ app/src/main/java/com/electriccouriers/bass/adapters/CardHistoryListAdapter.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.electriccouriers.bass.R;
@@ -35,6 +36,29 @@ public class CardHistoryListAdapter extends ArrayAdapter<History> {
 
         TextView routePointTitle = convertView.findViewById(R.id.Card_StationName);
         routePointTitle.setText(historyItem.getRoutePointName());
+
+        TextView routeTime = convertView.findViewById(R.id.Card_Time);
+        routeTime.setText(historyItem.getTime());
+
+        TextView routeDate = convertView.findViewById(R.id.Card_Date);
+        routeDate.setText(historyItem.getDate());
+
+        TextView routeInOut = convertView.findViewById(R.id.Card_InOut);
+        int io = historyItem.getStatus();
+        if (io == 0){
+            routeInOut.setText("in");
+        } else if (io == 1) {
+            routeInOut.setText("out");
+        }
+
+        ImageView routeType = convertView.findViewById(R.id.Card_Pictogram);
+        int t = historyItem.getType();
+        if (t == 1){
+            routeType.setImageResource(R.drawable.ic_business);
+        } else if (t == 0){
+            routeType.setImageResource(R.drawable.ic_home);
+        }
+
 
         return convertView;
     }
