8508d57cb1e3239f0fdc28e27a861f7076f81f0b
Thomas <tjp.hopstaken@ad-academie.nl>
2019-03-25 13:00:48 +0100

    Updated shuttle request Activity
    - added new shuttle image
    - added Activity to manifest
    - updated layout resource

--- app/src/main/java/com/electriccouriers/bass/activities/RequestActivity.java
+++ app/src/main/java/com/electriccouriers/bass/activities/RequestActivity.java
@@ -1,6 +1,7 @@
 package com.electriccouriers.bass.activities;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -21,12 +22,14 @@ public class RequestActivity extends BaseActivity {
 
         beginLocationButton = (Button) findViewById(R.id.beginlocation);
         beginLocationButton.setOnClickListener(v -> {
+            Log.e("log", "click");
             PopupMenu pm = new PopupMenu(RequestActivity.this, beginLocationButton);
             pm.getMenuInflater().inflate(R.menu.popup_menu, pm.getMenu());
             pm.setOnMenuItemClickListener(item -> {
                 switch (item.getItemId()) {
                     case R.id.first:
                         Toast.makeText(RequestActivity.this, "Geselecteerd!", Toast.LENGTH_SHORT).show();
+                        beginLocationButton.setText("Roosendaal Station");
                         return true;
 
                     case R.id.second:
@@ -40,6 +43,7 @@ public class RequestActivity extends BaseActivity {
 
                 return true;
             });
+            pm.show();
         });
     }
 
@@ -50,12 +54,12 @@ public class RequestActivity extends BaseActivity {
 
     @Override
     protected int getToolbarTitle() {
-        return 0;
+        return R.string.login_toolbar_title;
     }
 
     @Override
     protected int getToolbarNavigationIcon() {
-        return 0;
+        return R.drawable.ic_back_white;
     }
 
     @Override
