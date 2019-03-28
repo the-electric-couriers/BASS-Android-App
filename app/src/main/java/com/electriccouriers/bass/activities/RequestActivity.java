5d588d5c6fe7faae8411a0dff7eabab56ba8e4eb
Thomas <tjp.hopstaken@ad-academie.nl>
2019-03-28 11:31:21 +0100

    Improved time picker

--- app/src/main/java/com/electriccouriers/bass/activities/RequestActivity.java
+++ app/src/main/java/com/electriccouriers/bass/activities/RequestActivity.java
@@ -1,56 +1,28 @@
 package com.electriccouriers.bass.activities;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.PopupMenu;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.electriccouriers.bass.R;
 
+import java.util.Calendar;
+
 public class RequestActivity extends BaseActivity {
 
-    Button beginLocationButton;
-    Button5 time;
-    EditText time;
-
-
-  @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_request);
-
-        time = (button5) findViewById(R.id.time);
-        time.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Calendar mcurrentTime = Calendar.getInstance();
-                int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
-                int minute = mcurrentTime.get(Calendar.MINUTE);
-                TimePickerDialog mTimePicker;
-                mTimePicker = new TimePickerDialog(RequestActivity.this, new TimePickerDialog.OnTimeSetListener() {
-
-                    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
-                        time.setText(selectedHour + ":" + selectedMinute);
-                    }
-                }, hour, minute, true);
-                mTimePicker.setTitle("Ophaaltijd");
-                mTimePicker.show();
-
-            }
-        });
-    }
+    Button beginLocationButton, time;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_request);
-
-        beginLocationButton = (Button) findViewById(R.id.beginlocation);
+        beginLocationButton = findViewById(R.id.beginlocation);
         beginLocationButton.setOnClickListener(v -> {
             Log.e("log", "click");
             PopupMenu pm = new PopupMenu(RequestActivity.this, beginLocationButton);
@@ -64,14 +36,10 @@ public class RequestActivity extends BaseActivity {
 
                     case R.id.second:
                         Toast.makeText(RequestActivity.this, "Geselecteerd!", Toast.LENGTH_SHORT).show();
-                        beginLocationButton.setText("De vos");
-
                         return true;
 
                     case R.id.third:
                         Toast.makeText(RequestActivity.this, "Geselecteerd!", Toast.LENGTH_SHORT).show();
-                        beginLocationButton.setText("Primark");
-
                         return true;
                 }
 
@@ -79,6 +47,25 @@ public class RequestActivity extends BaseActivity {
             });
             pm.show();
         });
+
+        time = findViewById(R.id.button5);
+        time.setOnClickListener(v -> {
+
+            Calendar mcurrentTime = Calendar.getInstance();
+            int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+            int minute = mcurrentTime.get(Calendar.MINUTE);
+            TimePickerDialog mTimePicker;
+            mTimePicker = new TimePickerDialog(RequestActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                @Override
+                public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                    time.setText(selectedHour + ":" + selectedMinute);
+                }
+            }, hour, minute, true);
+            mTimePicker.setTitle("Selecteer een tijdstip");
+            mTimePicker.show();
+
+        });
+
     }
 
 
